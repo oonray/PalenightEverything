95b506f6ff3c0d116ba063afba468c184910b933
Your Name <you@example.com>
2019-06-27 15:16:43 +0200

    added vundle install

--- install.sh
+++ install.sh
@@ -8,6 +8,9 @@ mkdir "$nv"
 mkdir "$colors"
 mkdir "$syntax"
 
+apt-get isntall git
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
 cp .bashrc $HOME
 cp .vimrc $HOME
 cp init.vim $nv
