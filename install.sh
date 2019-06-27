b9a8c64f7e131a3700aee0bf5e322143e3fc7319
Your Name <you@example.com>
2019-06-27 14:43:21 +0200

    theme

--- install.sh
+++ install.sh
@@ -1,9 +1,17 @@
-config = $home/.config
-nvim = $config/nvim/
-vim = $home/.vim
-colors = $vim/colors
-syntax = $vim/syntax
+config="$HOME/.config"
+nv="$config/nvim/"
+v="$HOME/.vim"
+colors="$v/colors"
+syntax="$v/syntax"
 
+mkdir "$nv"
+mkdir "$colors"
+mkdir "$syntax"
+
+cp .bashrc $HOME
+cp .vimrc $HOME
+cp init.vim $nv
+cp perfectdark.vim $colors
 
 
 
