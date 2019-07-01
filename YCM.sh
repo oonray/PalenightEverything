a528a72209c892eb78a026a9819d01bde9c639a7
Your Name <you@example.com>
2019-07-01 14:47:05 +0200

    Palenight and YCM install

---
+++ YCM.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo apt-get install cmake
+cd ~/.vim/bundle/YouCompleteMe && python3 ./install.py
--- install.sh
+++ install.sh
@@ -8,14 +8,15 @@ mkdir "$nv"
 mkdir "$colors"
 mkdir "$syntax"
 
-apt-get install -y git neofetch
+sudo apt-get install -y git neofetch vim neovim tmux
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
+cp ./settings-backup ~/.config/dconf/user
+cp .tmux.conf $HOME
 cp .bashrc $HOME
 cp .vimrc $HOME
 cp init.vim $nv
 cp perfectdark.vim $colors
-
 
 
 
