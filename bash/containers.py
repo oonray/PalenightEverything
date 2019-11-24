51fdcfb60c1dd03482baaf9d2c67dcfcf04fac2f
Your Name <you@example.com>
2019-11-24 22:34:01 +0100

    .

---
+++ bash/containers.py
@@ -0,0 +1,15 @@
+#!python3
+
+import os, system, subprocess
+
+containers="/opt/Docker/"
+plex=$containers+"Plex/"
+minecraft=$containers+"Minecraft/"
+
+
+def start_containers(path):
+    subprocess.Popen()
+
+
+if __name__ == "__name__":
+    pass
